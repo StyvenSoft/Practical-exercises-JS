8b8414d609992fe207e330f44cd38747faa0510d
Steveen <steveen.echeverri@gmail.com>
2020-01-22 23:12:56 -0500

    Template design and error in transitions

--- 01-Drum-Kit/main.js
+++ 01-Drum-Kit/main.js
@@ -1,6 +1,21 @@
-window.addEventListener('keydown', function(e){
+
+const playSound = (e) =>{
     //console.log(e);
-    const audio = document.querySelector(`audio[data-key=${keyCode}]`);
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return;
+   
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+}
+
+const removetransition = (e) =>{
+    if(e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
 
 
-})
+const keys = Array.from(document.querySelector('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
--- Readme.md
+++ Readme.md
@@ -1,3 +1,5 @@
 # Practical exercises Javascript
 
 ## List of projects created to strengthen my skills
+
+- 1 Excercise info:  http://keycode.info/
